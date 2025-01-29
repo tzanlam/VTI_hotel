099f1006666fe41c195c730cbeabddbc8f93983e
tzanlam <tzanlam@gmail.com>
2025-01-29 13:34:25 +0700

    update mail

--- vti_hotel_be/src/main/java/hotel/vti_hotel/service/mailSender/IMailSender.java
+++ vti_hotel_be/src/main/java/hotel/vti_hotel/service/mailSender/IMailSender.java
@@ -1,13 +1,12 @@
 package hotel.vti_hotel.service.mailSender;
 
-import hotel.vti_hotel.modal.entity.Account;
 import hotel.vti_hotel.modal.request.MailSenderRequest;
 import jakarta.mail.MessagingException;
 
 public interface IMailSender {
     void mailSendCodeConfirm(MailSenderRequest request, String codeConfirm) throws MessagingException;
     String mailSendForgotPassword(MailSenderRequest request) throws MessagingException;
-    String mailInformationBooking(int bookingId, MailSenderRequest request) throws MessagingException;
+    void mailInformationBooking(int bookingId, MailSenderRequest request) throws MessagingException;
     String mailCanceledBooking(int bookingId, MailSenderRequest request) throws MessagingException;
     String mailInformationFastBooking(int fastBookingId, MailSenderRequest request) throws MessagingException;
     String mailCanceledFastBooking(int fastBookingId, MailSenderRequest request) throws MessagingException;
